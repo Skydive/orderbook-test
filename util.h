75dec90fb091f6d7aecd665f4282247dabefb27b
Khalid Aleem <me@precess.io>
2022-09-16 15:59:34 +0200

    Fixed bug with clang++

--- util.h
+++ util.h
@@ -14,13 +14,13 @@ using namespace std;
 namespace util {
     static inline void ltrim(string &s) {
         s.erase(s.begin(), find_if(s.begin(), s.end(),
-                not1(ptr_fun<int, int>(isspace))));
+            [](unsigned char c){return !std::isspace(c);}));
     }
 
     // trim from end (in place)
     static inline void rtrim(string &s) {
         s.erase(find_if(s.rbegin(), s.rend(),
-                not1(ptr_fun<int, int>(isspace))).base(), s.end());
+            [](unsigned char c){return !std::isspace(c);}).base(), s.end());
     }
 
     // trim from both ends (in place)
